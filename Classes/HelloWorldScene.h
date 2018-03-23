1a229cebcd1b70355484b917afd033926ef49f62
DiedRadish <diedradish@gmail.com>
2018-03-23 22:53:47 +0800

    Default project.

---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,26 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld: public cocos2d::Scene {
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	//callback
+	void onPress(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	void onRelease(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	// implement the "static create()" method manually
+	CREATE_FUNC(HelloWorld);
+
+private:
+	cocos2d::Sprite * sp;
+	cocos2d::MoveTo* mt;
+};
+
+#endif // __HELLOWORLD_SCENE_H__
